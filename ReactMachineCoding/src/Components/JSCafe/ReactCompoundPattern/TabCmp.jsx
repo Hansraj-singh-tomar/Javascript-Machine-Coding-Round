bd9d31d99755b9d1836be2074cdd865c0f060f0b
Hansraj_singh_tomar <hansrajput033@gmail.com>
2023-08-16 23:44:37 +0530

    files add in react machine coding folder

---
+++ ReactMachineCoding/src/Components/JSCafe/ReactCompoundPattern/TabCmp.jsx
@@ -0,0 +1,11 @@
+// Compound Pattern
+// 1. Multiple Components come together to serve a common functionality
+import React from 'react'
+
+const TabCmp = () => {
+  return (
+    <div>TabCmp</div>
+  )
+}
+
+export default TabCmp
